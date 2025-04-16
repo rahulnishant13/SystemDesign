fd084b7e35902ddea9e59885fac4beeb0c8fcfee
Rahul Nishant <rahulnishant13@gmail.com>
2025-04-16 12:57:05 +0530

    Rate limiter

---
+++ RateLimiter/Application.java
@@ -0,0 +1,79 @@
+package RateLimiter;
+
+public class Application {
+    public static void main(String[] args) throws InterruptedException {
+        FixedWindow rateLimiter =  FixedWindow.getInstance(3, 1000); // 3 requests per 1 second
+
+        String clientId = "user123";
+
+        for (int i = 0; i < 5; i++) {
+            if (rateLimiter.grantAccess(clientId)) {
+                System.out.println("Request " + (i + 1) + " allowed.");
+            } else {
+                System.out.println("Request " + (i + 1) + " blocked.");
+            }
+            Thread.sleep(300); // Simulate requests coming in at different times
+        }
+
+        Thread.sleep(1000); // Wait for the window to reset
+
+        // New window, should allow more requests
+        for (int i = 5; i < 7; i++) {
+            if (rateLimiter.grantAccess(clientId)) {
+                System.out.println("Request " + (i + 1) + " allowed.");
+            } else {
+                System.out.println("Request " + (i + 1) + " blocked.");
+            }
+            Thread.sleep(300);
+        }
+
+        // =========================================================================================
+
+        SlidingWindow slidingRateLimiter = SlidingWindow.getInstance(5, 1000); // 5 requests per 1 second
+        String clientIdS = "user456";
+
+        for (int i = 0; i < 10; i++) {
+            if (slidingRateLimiter.grantAccess(clientIdS)) {
+                System.out.println("Request " + (i + 1) + " allowed at " + System.currentTimeMillis());
+            } else {
+                System.out.println("Request " + (i + 1) + " blocked at " + System.currentTimeMillis());
+            }
+            Thread.sleep(200); // Simulate requests every 200ms
+        }
+        Thread.sleep(1000);
+        System.out.println("Starting new window");
+        for (int i = 10; i < 15; i++) {
+            if (slidingRateLimiter.grantAccess(clientIdS)) {
+                System.out.println("Request " + (i + 1) + " allowed at " + System.currentTimeMillis());
+            } else {
+                System.out.println("Request " + (i + 1) + " blocked at " + System.currentTimeMillis());
+            }
+            Thread.sleep(100);
+        }
+
+        // =========================================================================================
+
+        TokenBucket tokenRateLimiter = TokenBucket.getInstance(5, 200);
+        String clientIdT = "user123";
+
+        for (int i = 0; i < 15; i++) {
+            if (tokenRateLimiter.grantAccess(clientIdT)) {
+                System.out.println("Request " + (i + 1) + " allowed at " + System.currentTimeMillis());
+            } else {
+                System.out.println("Request " + (i + 1) + " blocked at " + System.currentTimeMillis());
+            }
+            Thread.sleep(100);
+        }
+
+        System.out.println("\nTesting with a different client, should use the same rate limiter instance:");
+        String clientId2 = "user456";
+        for (int i = 0; i < 3; i++) { //show that the rate limiter works for another client
+            if (tokenRateLimiter.grantAccess(clientId2)) {
+                System.out.println("Request " + (i + 1) + " allowed for " + clientId2 + " at " + System.currentTimeMillis());
+            } else {
+                System.out.println("Request " + (i + 1) + " blocked for " + clientId2 + " at " + System.currentTimeMillis());
+            }
+            Thread.sleep(500);
+        }
+    }
+}
---
+++ RateLimiter/SlidingWindow.java
@@ -0,0 +1,52 @@
+package RateLimiter;
+
+import java.awt.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class SlidingWindow implements RateLimiter {
+    private static SlidingWindow slidingWindow;
+    private long timeWindowInMilis;
+    private int bucketToken;
+    private ConcurrentHashMap<String, ConcurrentLinkedQueue<Long>> requestLog;
+
+    private SlidingWindow(int token, long timeWindowInMilis){
+        this.timeWindowInMilis = timeWindowInMilis;
+        this.bucketToken = token;
+        this.requestLog = new ConcurrentHashMap<>();
+    }
+
+    public static synchronized SlidingWindow getInstance(int token, long timeWindowInMilis) {
+        if(slidingWindow == null) {
+            slidingWindow = new SlidingWindow(token, timeWindowInMilis);
+        } else if(slidingWindow.bucketToken != token || slidingWindow.timeWindowInMilis != timeWindowInMilis) {
+//            check to throw error or reset it
+        }
+
+        return slidingWindow;
+    }
+
+    @Override
+    public boolean grantAccess(String clientId) {
+        long currTime = System.currentTimeMillis();
+        ConcurrentLinkedQueue<Long> queue = requestLog.computeIfAbsent(clientId, k -> new ConcurrentLinkedQueue<>());
+
+        while (true) {
+            Long head = queue.peek();
+            if(head != null && currTime - head > this.timeWindowInMilis) {
+                queue.poll();
+            } else {
+                break;
+            }
+        }
+
+        synchronized (queue) {
+            if(queue.size() < this.bucketToken) {
+                queue.offer(currTime);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
